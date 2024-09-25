ff4c46952dc4e3d36298e9578f11da4418847dd3
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-09-25 22:28:19 +0200

    ejercicios

--- Entregables/JS/entregable1.js
+++ Entregables/JS/entregable1.js
@@ -1,15 +1,21 @@
 //Crea una funcion que reciba dos cadenas y pinte cual es la más corta de las dos.
 //Si alguno de los dos argumentos no es una cadena devuelveu un mensaje de error en su lugar. 
 //Probarlo con varias entradas directamente introducidas con código. Las salidas se harán con console.log
-let cadena1 = "gato";
+let cadena1 = "gato es un animal salvaje";
 let cadena2 = "mañana es jueves";
 
-function compararCadenas(cadena1, cadena2){
-   if(typeof cadena1 !== "string" || typeof cadena2 !== "string"){
-      return console.log("Error: Alguno de los argumentos no es una cadena");
-   }
-    if(cadena1.length < cadena2.length){
-        return console.log(`${cadena1} es más corta que ${cadena2}`);
+function compararCadenas(cadena1, cadena2) {
+    if (typeof cadena1 !== "string" || typeof cadena2 !== "string") {
+        console.log("Error, no es una cadena");
+        return;
     }
-    return console.log(`${cadena2} es más corta que ${cadena1}`);
+
+    if (cadena1.length < cadena2.length) {
+        console.log(cadena1);
+    } else if (cadena2.length < cadena1.length) {
+        console.log(cadena2);
+    } else {
+        console.log("Ambas cadenas tienen la misma longitud");
+    }
 }
+compararCadenas(cadena1, cadena2);
