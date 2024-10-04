0c7834715e180ff0a97ebb4136516e29a3b0547e
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-10-04 11:01:34 +0200

    js

--- Tema3/Ejercicio3/js/e3.js
+++ Tema3/Ejercicio3/js/e3.js
@@ -1,3 +1,15 @@
+
+//1
+let uno = document.getElementsByTagName("a");
+for(let i = 0; i< uno.length; i++){
+    console.log(uno[i].textContent);
+    }
+
+
 function cambiarATwitter(){
-let twitter = document.querySelector('#aTwitter');
+let twitter = document.querySelector('#twitter');
 }
+
+cambiarATwitter();
+
+
