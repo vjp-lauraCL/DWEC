04062d50a9fc2e526ec957cdb237d5e2d49280e1
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-10-10 11:04:19 +0200

    botones

--- Tema3/ejercicio6_1/ej6_1.js
+++ Tema3/ejercicio6_1/ej6_1.js
@@ -1,18 +1,42 @@
-let colores =["rojo", "rosa", "verde", "amarillo", "azul", "violeta", "naranja"];
-let matriz = new Array(10);
-let elemento = document.querySelector("div");
+document.addEventListener("DOMContentLoaded", function() {
+    let colores = ["red", "pink", "green", "yellow", "blue", "violet", "orange"];
+    let contenedor = document.createElement("div");
+    contenedor.style.display = "grid";
+    contenedor.style.gridTemplateColumns = "repeat(10, 25px)";
+    document.body.appendChild(contenedor);
 
-    for(let i=0; i<10; i++){
-        matriz[i] = new Array(10);
-        for(let j=0; j<10; j++){
-
-            let boton = document.createElement("button");
-            elemento.style.width = "25px";
-            elemento.style.height = "25px";
-            elemento.style.margin = "10px";
-            elemento.style.border="2px solid black";
-            elemento.id = `${i}-${j}`;
-            matriz[i][j] = boton;
+    function crearMatriz() {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                let boton = document.createElement("button");
+                boton.style.width = "25px";
+                boton.style.height = "25px";
+                boton.style.margin = "0";
+                boton.style.border = "2px solid black";
+                boton.style.backgroundColor = colores[0];
+                boton.id = `${i}-${j}`;
+                contenedor.appendChild(boton);
+                asignarEventos(boton);
+            }
         }
     }
-console.log(matriz);
+    function asignarEventos(boton) {
+        boton.addEventListener("mousedown", function(event) {
+            event.preventDefault();
+            let colorActual = boton.style.backgroundColor;
+            let indice = colores.indexOf(colorActual);
+            if (event.button === 0) { // Botón izquierdo
+                boton.style.backgroundColor = colores[(indice + 1) % colores.length];
+            } else if (event.button === 2) { // Botón derecho
+                boton.style.backgroundColor = colores[(indice - 1 + colores.length) % colores.length];
+            } else if (event.button === 1) { // Botón de la ruleta
+                boton.style.backgroundColor = "gray";
+            }
+        });
+        boton.addEventListener("contextmenu", function(event) {
+            event.preventDefault();
+        });
+    }
+
+    crearMatriz();
+});
